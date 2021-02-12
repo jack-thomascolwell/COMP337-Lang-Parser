92a75ab8f9b05877b348de07fa7150261e1e8ad4
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2021-02-12 16:41:20 -0500

    Moved parse to own file and added interpreter.py

---
+++ parse.py
@@ -0,0 +1,21 @@
+class Parse:
+
+    def __init__(self, type, index, *children):
+        self.type = type
+        self.index = index
+        self.children = children
+
+    def __eq__(self, other):
+        if (not (isinstance(other, Parse) and self.index == other.index and self.type == other.type)):
+            return False
+        for child, otherChild in zip(self.children, other.children):
+            if (child != otherChild):
+                return False
+        return True
+
+    def __str__(self):
+        string = '(%s '%self.type
+        for child in self.children:
+            string += str(child) + ' '
+        string = string[:-1] + ')'
+        return string
